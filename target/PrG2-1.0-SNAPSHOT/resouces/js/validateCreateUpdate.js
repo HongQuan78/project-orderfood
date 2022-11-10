a6fabad6380c19d4d7f30495732e3abc3f4498cd
Quanvo313 <vhquan313@gmail.com>
2022-11-10 16:07:55 +0700

    v

--- target/PrG2-1.0-SNAPSHOT/resouces/js/validateCreateUpdate.js
+++ target/PrG2-1.0-SNAPSHOT/resouces/js/validateCreateUpdate.js
@@ -41,3 +41,18 @@ jQuery.validator.addMethod("checkPhone", function (value, element) {
     // allow any non-whitespace characters as the host part
     return this.optional(element) || /^((\(0[1-9]\d{0,2}\)|0[1-9]\d{0,2}[\.\-]?)\d{7,8}|0[1-9]\d{8,9})$/.test(value);
 }, 'Please enter a valid phone number.');
+
+$(function () {
+    var dtToday = new Date();
+
+    var month = dtToday.getMonth() + 1;// jan=0; feb=1 .......
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear() - 18;
+    if (month < 10)
+        month = '0' + month.toString();
+    if (day < 10)
+        day = '0' + day.toString();
+    var minDate = year + '-' + month + '-' + day;
+    var maxDate = year + '-' + month + '-' + day;
+    $('#birthdayDate').attr('max', maxDate);
+});
